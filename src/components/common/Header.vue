dc6704433b6af72879f20f1244a2988e156f36a0
jiabaochuan <850289300@qq.com>
2022-03-29 16:07:52 +0800

    feat: add header

---
+++ src/components/common/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="common-bc-header"></div>
+</template>
+<script lang="ts" setup>
+</script>
+<style lang="less" scoped>
+.common-bc-header {
+  height: 48px;
+  width: 100%;
+  background: #081b37;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid rgba(1, 36, 62, 0.1);
+}
+</style>
